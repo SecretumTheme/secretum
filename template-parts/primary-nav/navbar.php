2d82a6aa34e7772ee7d9327f0cbb800cdd5a8cc5
SecretumTheme <chrisw@null.net>
2020-08-02 15:35:20 -0400

    Moved default no-nav fallback into template

--- template-parts/primary-nav/navbar.php
+++ template-parts/primary-nav/navbar.php
@@ -3,49 +3,45 @@
  * Display Primary Nav Menu
  *
  * @package    Secretum
- * @subpackage Template-Parts
+ * @subpackage Theme
  * @author     SecretumTheme <author@secretumtheme.com>
- * @copyright  2018-2019 Secretum
+ * @copyright  2018-2020 Secretum
  * @license    https://github.com/SecretumTheme/secretum/blob/master/license.txt GPL-2.0
- * @link       https://github.com/SecretumTheme/secretum/blob/master/template-parts/primary-nav/wp-nav-menu.php
+ * @link       https://github.com/SecretumTheme/secretum/blob/master/template-parts/primary-nav/navbar.php
  * @since      1.6.0
  */
 
 namespace Secretum;
 
-$secretum_primary_nav_theme_location = 'secretum-navbar-primary';
-if ( true !== has_nav_menu( 'secretum-navbar-primary' ) && true === has_nav_menu( 'secretum-navbar-primary-below' ) ) {
-	$secretum_primary_nav_theme_location = 'secretum-navbar-primary-below';
+if ( false !== secretum_mod( 'primary_nav_status' ) ) {
+	wp_nav_menu(
+		array(
+			'depth'           => 5,
+			'theme_location'  => secretum_navbar_primary_nav_location(),
+			'container_class' => secretum_navbar_toggler_display( 'class' ),
+			'container_id'    => 'navbarNavDropdown',
+			'menu_class'      => 'navbar-nav' . secretum_alignment( 'primary_nav', 'return', array( 'margin' => true ) ),
+			'menu_id'         => 'main-menu',
+			'divider'         => secretum_nav_item( 'primary_nav' ),
+			'walker'          => new \Secretum\Navwalker(
+				'primary_nav',
+				secretum_nav_dropdown( 'primary_nav' ),
+				secretum_textual( 'primary_nav_dropdown', 'return' )
+			),
+			'fallback_cb'     => '\Secretum\Navwalker::fallback',
+			'echo'            => true,
+		)
+	);
+
+	// Default no primary menu set, and menu status active.
+	if ( false === has_nav_menu( 'secretum-navbar-primary' ) && false === has_nav_menu( 'secretum-navbar-primary-below' ) ) {
+		wp_page_menu(
+			array(
+				'show_home'  => true,
+				'include'    => 99999,
+				'before'     => '<ul id="main-menu" class="navbar-nav">',
+				'menu_class' => 'navbar-nav' . secretum_alignment( 'primary_nav', 'return', array( 'margin' => true ) ),
+			)
+		);
+	}
 }
-
-?>
-<nav class="wrapper navbar <?php secretum_navbar_toggler_display( 'size' ); ?><?php secretum_wrapper( 'primary_nav' ); ?>" aria-label="<?php secretum_text( 'primary_nav_aria_label_text', true ); ?>" role="navigation">
-<div class="container<?php secretum_container( 'primary_nav', 'echo', [ 'textuals' => true ] ); ?>">
-<?php
-if ( false !== secretum_mod( 'primary_nav_toggler_status' ) ) {
-	get_template_part( 'template-parts/primary-nav/toggler' );
-}
-
-wp_nav_menu(
-	[
-		'depth'           => 5,
-		'theme_location'  => $secretum_primary_nav_theme_location,
-		'container_class' => secretum_navbar_toggler_display( 'class' ),
-		'container_id'    => 'navbarNavDropdown',
-		'menu_class'      => 'navbar-nav' . secretum_alignment( 'primary_nav', 'return', [ 'margin' => true ] ),
-		'menu_id'         => 'main-menu',
-		'divider'         => secretum_nav_item( 'primary_nav' ),
-		'walker'          => new \Secretum\Navwalker(
-			'primary_nav',
-			secretum_nav_dropdown( 'primary_nav' ),
-			secretum_textual( 'primary_nav_dropdown', 'return' )
-		),
-		'fallback_cb'     => '\Secretum\Navwalker::fallback',
-		'echo'            => true,
-	]
-);
-
-get_template_part( 'template-parts/primary-nav/search' );
-?>
-</nav><!-- .navbar -->
-</div><!-- .col-md -->
